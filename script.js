fa9450696c07f964a67f1905e57ded584db7fb24
VTheGreek <vasilis.proefth@gmail.com>
2026-01-02 00:50:48 +0200

    added operation possibilities in case of letter instead of number and division with 0

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let num = Number(prompt("Choose any number."))
+let num = Number(prompt("Choose any number."));
 let operator = prompt("Choose any operator '+', '-', '*', '/', '**', '%'");
-let num2 = Number(prompt("Choose another one number."))
+let num2 = Number(prompt("Choose another one number."));
 
-
-if (operator === "+") {
-    alert(num + num2);
-} else if (operator === "-") {
-    alert(num - num2); 
-} else if (operator === "*") {
-    alert(num * num2); 
-} else if (operator === "/") {
-    alert(num / num2);
-} else if (operator === "**") {
-    alert(num ** num2);
-} else if (operator === "%") {
-    alert(num % num2);
+if (isNaN(num)) {
+  alert(`${num} is not a number`);
+} else if (isNaN(num2)) {
+  alert(`${num2} is not a number`);
 } else {
-    alert("The operation is not possible");
+  if ((operator === "/" || operator === "%") && num2 === 0) {
+    alert("Division with 0 is always infinity.");
+  } else {
+    if (operator === "+") {
+      alert(num + num2);
+    } else if (operator === "-") {
+      alert(num - num2);
+    } else if (operator === "*") {
+      alert(num * num2);
+    } else if (operator === "/") {
+      alert(num / num2);
+    } else if (operator === "**") {
+      alert(num ** num2);
+    } else if (operator === "%") {
+      alert(num % num2);
+    } else {
+      alert("The operation is not possible");
+    }
+  }
 }
